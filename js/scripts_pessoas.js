3f2997e7d7275356da47f3675f6b142b90b3a650
marcella <marcellamatos2428@gmail.com>
2026-06-22 14:51:21 -0300

    dados das pessoas no formulario

---
+++ js/scripts_pessoas.js
@@ -0,0 +1,35 @@
+//pegando elementos do dom
+const formpessoa = document.querySelector("form-pessoa")
+const divlista = document.querySelector("#div-lista-pessoas")
+
+//criando array pessoas
+
+const pessoas = []
+
+//capturar o evento submit do formulario
+formpessoa.addEventListener('submit', (evt) => {
+    //interrompe o efeito de submeter dos dados do formulario
+    evt.preventDefault()
+
+    //criar um objeto formulario
+    const dadosformpessoa = new FormData(formpessoa)
+
+    //criar um objeto literal 
+    const pessoa = {
+        nome: dadosformpessoa.get('nome'),
+        idade: dadosformpessoa.get('idade'),
+        renda: dadosformpessoa.get("renda")
+    }
+
+    //chamando a funçao addpessoa e passando o objeto literal pessoa 
+    addpessoa(pessoa)
+})
+
+//criando a funçao ADICIONAR pessoa
+
+const addpessoa = (objpessoa) => {
+    pessoas.push(objpessoa)
+}
+
+
+
