644bdf2d474188408d56c2c41e6c52f2518e2f0d
marcella <marcellamatos2428@gmail.com>
2026-06-22 16:51:03 -0300

    resultcalc alterado

--- js/scripts_calculos.js
+++ js/scripts_calculos.js
@@ -5,13 +5,13 @@ const calcdesconto = (objpessoa) => {
     if((objpessoa.idade < 18) || (objpessoa.renda <=1700)){
         resultcalc = 'isento de desconto'
     }else if(objpessoa.renda <= 5000){
-        resultcalc = objpessoa.renda * 0.05
+        resultcalc = ` R$ ${parseFloat(objpessoa.renda * 0.05).toFixed(2).replace('.',',')}`
     }else if(objpessoa.renda <= 8000){
-        resultcalc = objpessoa.renda * 0.10
+        resultcalc = `R$ ${parseFloat(objpessoa.renda * 0.10).toFixed(2).replace('.',',')}`
     }else if(objpessoa.renda <= 12000){
-        resultcalc = objpessoa.renda * 0.15
+        resultcalc = `R$ ${parseFloat(objpessoa.renda * 0.15).toFixed(2).replace('.',',')}`
     }else{
-        resultcalc = objpessoa.renda * 0.20
+        resultcalc = `R$ ${parseFloat(objpessoa.renda * 0.20).toFixed(2).replace('.',',')}`
 
     }
     return resultcalc
